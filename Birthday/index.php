2f665479558463629d552d7f97cbf587e1d168ac
Jessica Harris <jessicarose_10@hotmail.com>
2016-12-11 21:43:46 -0800

    added some structure to the file locations

---
+++ Birthday/index.php
@@ -0,0 +1,22 @@
+<?php
+    $years = scandir("years");
+    var_dump($years);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Birthday Card</title>
+        <link href="index.css" rel="stylesheet" type="text/css" />
+    </head>
+
+    <body>
+        <div id="header" class="banner">
+            <h1>Happy Birthday</h1>
+        </div>
+        <div id="footer" class="banner">
+            <p>
+                You're the best!
+            </p>
+        </div>
+    </body
+</html>
