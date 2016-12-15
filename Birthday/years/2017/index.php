c9ae312c89421823e10ec4a0e6bb489e59d496b1
Kyle Reddeman <kyler7662@gmail.com>
2016-12-15 15:08:00 -0800

    -added curved edges to years -added a little code to next year -worked on stupid footers -small style changes

---
+++ Birthday/years/2017/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Happy Birthday!</title>
+        <link href="card.css" type="text/css" rel="stylesheet" />
+        <link href="../../pictures/2015_2016/cake.png" type="image/gif" rel="shortcut icon" />
+    </head>
+    <body>
+
+    </body>
+</html>
